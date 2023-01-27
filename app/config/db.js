d867add3f4b504b23d262ea56ffa7fbf68daa649
satyadeep-neupane <satyadeep.neupane@deerwalk.edu.np>
2023-01-27 16:03:31 +0545

    DB addedd

--- app/config/db.js
+++ app/config/db.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
+        mongoose.set('strictQuery', true);
         const conn = await mongoose.connect(process.env.DATABASE_URL)
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (err) {
